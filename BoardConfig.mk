8ae108fb1d50efa22542065b251718c9877ae506
Arnav Gupta <championswimmer@aokp.co>
2014-05-06 02:30:02 +0530

    amami: define dts config name
    Change-Id: Ife758165994d246aee6b8b8c323688381269f8f2
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/amami/bluetooth
 
 # Kernel properties
 TARGET_KERNEL_CONFIG := cm_rhine_amami_row_defconfig
+SOMC_DTS_TARGET := rhine_amami_row
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
