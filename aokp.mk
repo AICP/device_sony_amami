e76d06233b80de997f2fc386f8d377395c55ad52
Omar Einea <eineao@gmail.com>
2014-07-20 01:17:23 +0300

    amami: Welcome to AICP

--- aokp.mk
+++ aokp.mk
@@ -1,12 +1,14 @@
-# Inherit common product files.
-$(call inherit-product, vendor/aokp/configs/common.mk)
+# Inherit AICP common Phone stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Inherit GSM common stuff
-$(call inherit-product, vendor/aokp/configs/gsm.mk)
+$(call inherit-product, vendor/aicp/configs/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
 # Inherit AOSP device configuration for amami
 $(call inherit-product, device/sony/amami/full_amami.mk)
-
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5503 BUILD_FINGERPRINT=Sony/D5503/D5503:4.3/14.2.A.1.142/cfv_jg:user/release-keys PRIVATE_BUILD_DESC="D5503-user 4.3 RHINE-1.1.1-140124-0137 36 test-keys"
 
--- full_amami.mk
+++ full_amami.mk
@@ -16,7 +16,7 @@
 
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
-
+ 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
