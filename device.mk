4acb8116ff602e3bea4c7d5d2825fae85a6d6a06
SpiritCroc <spiritcroc@gmail.com>
2016-09-07 13:34:33 +0200

    AICPfy
    Change-Id: I6378c053769fdd652acf940a5ecc400c48e684f2

--- device.mk
+++ device.mk
@@ -54,5 +54,5 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.usb.pid_suffix=1A7
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, device/sony/rhine/platform.mk)
+$(call inherit-product, device/sony/rhine-common/platform.mk)
 $(call inherit-product, vendor/sony/rhine-amami/amami-vendor.mk)
